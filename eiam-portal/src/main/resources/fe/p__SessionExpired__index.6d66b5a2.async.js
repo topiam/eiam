fd8315cf5bbbc06fc2767906775308e7e12f5e6d
topiam <support@topiam.cn>
2022-12-18 16:42:04 +0800

    :zap: 前端样式兼容性调整

--- eiam-portal/src/main/resources/fe/p__SessionExpired__index.6d66b5a2.async.js
+++ eiam-portal/src/main/resources/fe/p__SessionExpired__index.6d66b5a2.async.js
@@ -1,1 +1,18 @@
+/*
+ * eiam-portal - Employee Identity and Access Management Program
+ * Copyright © 2020-2022 TopIAM (support@topiam.cn)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 "use strict";(self.webpackChunktopiam_portal=self.webpackChunktopiam_portal||[]).push([[26],{24041:function(c,n,_){_.r(n);var i=_(97983),a=_.n(i),u=_(11281),r=_.n(u),d=_(40794),t=_.n(d),s=_(40367),e=_(84865),v=_(78234),j=_(69400),U=_(17673),I=_(67038),y=_(98971),E=_(85893);n.default=function(){var P=(0,y.useModel)("@@initialState"),C=P.initialState,T=P.setInitialState;return(0,v.Z)(t()(a()().mark(function h(){return a()().wrap(function(f){for(;;)switch(f.prev=f.next){case 0:j.Z.warning({title:"\u4F1A\u8BDD\u8FC7\u671F",content:"\u60A8\u7684\u767B\u5F55\u4FE1\u606F\u5DF2\u8FC7\u671F\uFF0C\u8BF7\u91CD\u65B0\u767B\u5F55\u3002",okText:"\u786E\u8BA4",okType:"danger",centered:!1,maskClosable:!1,okCancel:!1,onOk:function(){var b=t()(a()().mark(function R(){var M,O,o,l,p,D;return a()().wrap(function(m){for(;;)switch(m.prev=m.next){case 0:return m.next=2,T(r()(r()({},C),{},{currentUser:void 0}));case 2:M=(0,I.parse)(e.m.location.search),O=M,o=O.redirect_uri,l={pathname:s.wm},p=o&&o.split("/"),o&&o!==p[0]+"//"+p[2]+"/"&&(l=r()(r()({},l),{},{search:(0,U.stringify)({redirect_uri:o})})),D=e.m.createHref(l),window.location.replace(D);case 9:case"end":return m.stop()}},R)}));function B(){return b.apply(this,arguments)}return B}()});case 1:case"end":return f.stop()}},h)}))),(0,E.jsx)(E.Fragment,{})}},78234:function(c,n,_){var i=_(67294),a=_(92770),u=_(31663),r=function(t){u.Z&&((0,a.mf)(t)||console.error('useMount: parameter `fn` expected to be a function, but got "'.concat(typeof t,'".'))),(0,i.useEffect)(function(){t==null||t()},[])};n.Z=r},92770:function(c,n,_){_.d(n,{mf:function(){return a}});var i=function(e){return e!==null&&typeof e=="object"},a=function(e){return typeof e=="function"},u=function(e){return typeof e=="string"},r=function(e){return typeof e=="boolean"},d=function(e){return typeof e=="number"},t=function(e){return typeof e=="undefined"}},31663:function(c,n){var _=!1;n.Z=_}}]);
--- eiam-portal/src/main/resources/fe/p__Workplace__index.1e0cb992.async.js
+++ eiam-portal/src/main/resources/fe/p__Workplace__index.1e0cb992.async.js
@@ -1,1 +1,18 @@
+/*
+ * eiam-portal - Employee Identity and Access Management Program
+ * Copyright © 2020-2022 TopIAM (support@topiam.cn)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 "use strict";(self.webpackChunktopiam_portal=self.webpackChunktopiam_portal||[]).push([[297],{61430:function(u,r,a){a.r(r),a.d(r,{default:function(){return e}});var t=a(4393),o=a(99138),s=a(19847),n=a(85893),l=function(){return(0,n.jsx)(s._z,{children:(0,n.jsx)(t.Z,{children:(0,n.jsx)(o.Z,{banner:!0,description:"\u6B22\u8FCE\u4F7F\u7528 TopIAM \u4F01\u4E1A\u6570\u5B57\u8EAB\u4EFD\u7BA1\u63A7\u5E73\u53F0",type:"success"})})})},c=l,e=c}}]);
