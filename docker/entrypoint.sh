8820b5929793b3c2ed97fd14af67ab9e87336617
chenwen@trusfort.com <chenwen>
2023-02-07 13:55:53 +0800

    feature:添加单机版本docker-comspoe.yml

---
+++ docker/entrypoint.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+#
+# TopIAM Employee - Employee Identity and Access Management Program
+# Copyright © 2020-2023 TopIAM (support@topiam.cn)
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU Affero General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Affero General Public License for more details.
+#
+# You should have received a copy of the GNU Affero General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+java  -XX:TieredStopAtLevel=1 \
+      -Djava.security.egd=file:/dev/./urandom \
+      -Dspring.main.lazy-initialization=false \
+       org.springframework.boot.loader.JarLauncher
