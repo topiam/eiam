683e4106f2e8ee8f31460e437b7abb8f6e501971
smallbun <2689170096@qq.com>
2023-09-10 22:31:30 +0800

    :zap: 应用分组

---
+++ eiam-console/src/main/console-fe/src/pages/app/AppGroup/locales/zh-CN.ts
@@ -0,0 +1,40 @@
+/*
+ * eiam-console - Employee Identity and Access Management
+ * Copyright © 2022-Present Jinan Yuanchuang Network Technology Co., Ltd. (support@topiam.cn)
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+export default {
+  'pages.app_group.list.create': '创建分组',
+  'pages.app_group.list.column.name': '分组名称',
+  'pages.app_group.list.column.code': '分组编码',
+  'pages.app_group.list.column.create_time': '创建时间',
+  'pages.app_group.list.column.remark': '备注',
+  'pages.app_group.list.column.app_count': '应用数量',
+  'pages.app_group.list.column.type': '分组类型',
+  'pages.app_group.list.column.type.default': '系统默认',
+  'pages.app_group.list.column.type.custom': '自定义',
+  'pages.app_group.list.column.option': '操作',
+  'pages.app_group.list.actions.popconfirm.delete': '您确定要删除此应用分组？',
+  'pages.app_group.create.modal_form.title': '添加分组',
+  'pages.app_group.modal_form.name': '分组名称',
+  'pages.app_group.modal_form.name.placeholder': '请输入分组名称',
+  'pages.app_group.modal_form.name.rule.0.message': '分组名称为必填项',
+  'pages.app_group.modal_form.code': '分组编码',
+  'pages.app_group.modal_form.code.placeholder': '请输入分组编码',
+  'pages.app_group.modal_form.code.rule.0.message': '分组编码为必填项',
+  'pages.app_group.modal_form.remark': '备注',
+  'pages.app_group.modal_form.remark.placeholder': '请输入备注',
+  'pages.app_group.update.modal_form.title': '修改分组',
+};
