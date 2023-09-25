76905710edf8a567e2306ce192344e121cf5285c
shao1121353141 <>
2023-09-25 19:29:55 +0800

    优化

--- eiam-common/src/main/java/cn/topiam/employee/common/repository/app/impl/AppRepositoryCustomizedImpl.java
+++ eiam-common/src/main/java/cn/topiam/employee/common/repository/app/impl/AppRepositoryCustomizedImpl.java
@@ -58,7 +58,7 @@ public class AppRepositoryCustomizedImpl implements AppRepositoryCustomized {
      * 获取我的应用列表
      *
      * @param userId   {@link  Long}
-     * @param query {@link query}
+     * @param query {@link GetAppListQuery}
      * @param pageable {@link  String}
      * @return {@link List}
      */
